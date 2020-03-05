03caab067603a528c70bee4da81b310873d041e3
Anamika Panna <anamika2028@gmail.com>
2020-03-05 18:15:35 -0500

    Added mysql db connection

--- employeeTracker.js
+++ employeeTracker.js
@@ -1,23 +1,24 @@
+
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-    host: "localhost",
+  host: "localhost",
 
-    // Your port; if not 3306
-    port: 3306,
+  // Your port; if not 3306
+  port: 3306,
 
-    // Your username
-    user: "root",
+  // Your username
+  user: "root",
 
-    // Your password
-    password: "Mysql123",                             //process.env.MYSQL_KEY,                    
-    database: "employee_trackerDB"
+  // Your password
+  password: "Mysql123",                                            //process.env.MYSQL_KEY,    
+  database: "employee_trackerDB"
 });
 
-connection.connect(function (err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    afterConnection();
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  connection.end();
 });
 
 function afterConnection() {
