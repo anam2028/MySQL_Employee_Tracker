22b60b04f1516dd6212ad15c6a4dd82b29a7404a
Anamika Panna <anamika2028@gmail.com>
2020-03-06 11:50:15 -0500

    added methods for employee, dept, role

--- employeeTracker.js
+++ employeeTracker.js
@@ -16,63 +16,187 @@ var connection = mysql.createConnection({
   database: "employee_trackerDB"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
   connection.end();
 });
 
 function afterConnection() {
-    connection.query("SELECT * FROM employee", function (err, res) {
-        if (err) throw err;
-        console.log(res);
-        connection.end();
-    });
+  connection.query("SELECT * FROM employee", function (err, res) {
+    if (err) throw err;
+    console.log(res);
+    connection.end();
+  });
 }
 
-/*  Inquirer prompt for command line prompt */
+/*  Inquirer for command line prompt */
 
 inquirer.prompt([
-  
+
   {
-      type: "list",
-      name: "choose",
-      message: "Please choose:",
-      choices: ['Add Employee', 'Remove Employee', 'Update Employee Role', 'Update Employee Manager',
-      'View All Employees','View All Employess by Department','View All Employees by Manager']
+    type: "list",
+    name: "choose",
+    message: "What would you like to do?:",
+    choices: ['Add Department', 'Add Employee', 'Remove Employee', 'Update Employee Role', 'Update Employee Manager',
+      'View All Employees', 'View All Employess by Department', 'View All Employees by Manager']
   }
 ])
   .then(function (answers) {
 
-      if (answers.choose === "Add Employee") {
-          addEmployee();
-
-      }
-      else if (answers.choose === "View All Employees") {
-          joinById();
-
-      }
-      else if (answers.choose === 'Remove Employee')
-      {
-        joinById();
-      }
-
-      else if (answers.choose === "Update Employee Role") {
-        updateRole();
+    if (answers.choose === "Add Department") {
+      addDepartment();
     }
-      else if (answers.choose === "Update Employee Manager") {
-          updateEmployee();
-      }
-      else if (answers.choose === "View All  Employees by Department") {
-          joinByDepartmentId();
-      }
-      else if (answers.choose === "View All Employees by Manager"){
-          joinByManagerId();
-      }
-
-      else {
-          console.log("Thanks You for using this program!")
-      }
-
+    else if (answers.choose === "Add Employee Role") {
+      addEmployeeRole();
+    }
+    else if (answers.choose === "Add Employee") {
+      addEmployee();
+    }
+    else if (answers.choose === "View All Employees") {
+      readEmployees();
+    }
+    else if (answers.choose === 'Remove Employee') {
+      removeEmployee();
+    }
+    else if (answers.choose === "Update Employee Role") {
+      updateRole();
+    }
+    else if (answers.choose === "Update Employee Manager") {
+      updateEmployee();
+    }
+    else if (answers.choose === "View All  Employees by Department") {
+      joinByDepartmentId();
+    }
+    else if (answers.choose === "View All Employees by Manager") {
+      joinByManagerId();
+    }
+    else {
+      console.log("Thanks You for using this program!")
+    }
   })
 
+// Write a function for selections
+// refer iceCreamCRUD activity
+
+// functions for employee table
+
+function addEmployee() {
+  console.log("Adding a new employee...\n");
+  var query = connection.query(
+    "INSERT INTO employee SET ?",
+    {
+      id: 1,
+      first_name: "John",
+      last_name: "Doe",
+      role_id: 1,
+      manager_id: 1
+    },
+    function (err, res) {
+      if (err) throw err;
+      console.log(res.affectedRows + " New employee added!\n");
+      // Call updateEmployee AFTER the ADD completes
+      updateEmployeeRole();
+    }
+  );
+
+  // logs the actual query being run
+  console.log(query.sql);
+}
+
+
+function updateEmployeeRole() {
+  console.log("Updating employee role...\n");
+  var query = connection.query(
+    "UPDATE employee SET ? WHERE ?",
+    [
+      {
+        id: 100
+      },
+      {
+        role_id: 1
+      }
+    ],
+    function (err, res) {
+      if (err) throw err;
+      console.log(res.affectedRows + " employee role updated!\n");
+      // Call deleteProduct AFTER the UPDATE completes
+      deleteProduct();
+    }
+  );
+
+  // logs the actual query being run
+  console.log(query.sql);
+}
+
+function removeEmployee() {
+  console.log("Deleting all strawberry icecream...\n");
+  connection.query(
+    "DELETE FROM employee WHERE ?",
+    {
+      id: 1
+    },
+    function (err, res) {
+      if (err) throw err;
+      console.log(res.affectedRows + " employee removed from database!\n");
+      // Call readEmployee AFTER the DELETE completes
+      readEmployees();
+    }
+  );
+}
+
+function readEmployees() {
+  console.log("Selecting all employess...\n");
+  connection.query("SELECT * FROM employee", function (err, res) {
+    if (err) throw err;
+    // Log all results of the SELECT statement
+    console.log(res);
+    connection.end();
+  });
+}
+
+// write a function to add department
+
+function addDepartment() {
+  console.log("Adding department for a employee...\n");
+  var query = connection.query(
+    "INSERT INTO department SET ?",
+    {
+      id: 100,
+      name: "Sales"
+    },
+    function (err, res) {
+      if (err) throw err;
+      console.log(res.affectedRows + " Department added for employee!\n");
+      // Call updateEmployee AFTER the ADD completes
+      // updateEmployeeRole();
+    }
+  );
+
+  // logs the actual query being run
+  console.log(query.sql);
+}
+
+//write a function to add roles
+
+function addEmployeeRole() {
+  console.log("Adding employee role for a employee...\n");
+  var query = connection.query(
+    "INSERT INTO role SET ?",
+    {
+      id: 1,
+      department_id: 100,
+      title: "Sales",
+      salary: 100000
+    },
+    function (err, res) {
+      if (err) throw err;
+      console.log(res.affectedRows + " Employee role added!\n");
+      // Call updateEmployee AFTER the ADD completes
+      updateEmployeeRole();
+    }
+  );
+
+  // logs the actual query being run
+  console.log(query.sql);
+}
